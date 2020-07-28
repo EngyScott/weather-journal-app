4432acb9e3578e30d178d518aec9b3ae3dd58020
EngyScott <engy.scott@ymail.com>
2020-07-28 20:16:34 +0200

    Assign progectData as end point and inhance temp display

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const   express     = require('express'),
 
 
 // Setup empty JS object to act as endpoint for all routes
-let projectData = {
-  name: 'sami',
-  age: '20'
-};
+let projectData = {};
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -25,38 +22,19 @@ app.use(express.static('website'));
 app.get('/all', sendData);
 
 function sendData (req, res) {
-  res.send(data);
-  data=[]
+  res.send(projectData);
 };
 
-// POST data
-let data = [];
-
+// POST route
 app.post('/add', addData);
 
 function addData (req, res){
   let {date, temp, content} = req.body;
-    data.push(req.body);
-    return data;
+  projectData.date = date;
+  projectData.temp = temp;
+  projectData.content = content;
+    return projectData;
 };
-
-
-
-// // Post Route
-// app.post('/add', addData);
-
-// function addData(req, res) {
-//   let {date, temp, content} = req.body;
-//   projectData.push(req.body);
-//   res.send(projectData);
-// }
-
-// // Get Route
-// app.get('/', getData);
-
-// function getData(req, res) {
-//   res.send(projectData);
-// }
 
 // Setup Server
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- website/app.js
+++ website/app.js
@@ -4,74 +4,76 @@ const apiKey = '&appid=e11e5834b84ef1ecc09b19acf294ba59';
 // Create a new date instance dynamically with JS
 let d = new Date();
 const newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-// Generate fetching data by generate button click event listener;
+// Generate fetching functions by generate button click event;
 document.getElementById('generate').addEventListener('click', performAction);
 
-/* Trigger data functions on click event */
+/* Processing Functions */
 function performAction(event) {
   // Stop instant form supmission
   event.preventDefault();
   // get user input values
   const cityZip = document.getElementById('zip').value;
   const content = document.getElementById('feelings').value;
-  // 
+  // Fetch data from openweathermap with user credintials
   getWeather(baseURL, cityZip, apiKey)
+    // process data coming from endpoint
     .then(function (userInfo) {
-      // POST data
+      // POST data to data array
       postData('/add', { date: newDate, temp: userInfo.main.temp, content })
     })
+    // retrieve data array data to update HTML
     .then(function (newData) {
       // call updateUI to update browser content
       updateUI()
-    })
-  // reset form
-  
+    })  
 }
 
-/* Function to GET Web API Data*/
+/* Fetch openweathermap data with user credintials */
 const getWeather = async (baseURL, cityZip, apiKey) => {
-  // res equals to the result of fetch function
+  // assig res to the fetched data
   const res = await fetch(baseURL+cityZip+apiKey);
   try {
-    // assign fetched data to userInfo
+    // assign processed fetched data to userInfo
     const userInfo = await res.json();
+    // return user info
     return userInfo;
+    // Catch errors
   } catch (error) {
     console.log("error", error);
   }
 }
 
-/* Function to POST data */
+/* Send data to server*/
 const postData = async (url = '', data = {}) => {
+  // assign processed data to response & set credentials
   const res = await fetch(url, {
     method: "POST",
     credentials: "same-origin",
     headers: {
       "Content-Type": "application/json"
     },
+    // make data readable
     body: JSON.stringify(data)
   });
     try {
       const newData = await res.json();
-      console.log (newData);
+      // pass data on
       return newData;
+      // OR catch errors
     }catch (error) {
       console.log('error', error);
     }
 };
 
-
+/* Render fetched data on browser */
 const updateUI = async () => {
   const request = await fetch('/all');
   try {
     let allData = await request.json()
     // update HTML
-    document.getElementById('date').innerHTML = `Date: ${allData[0].date}`;
-    document.getElementById('temp').innerHTML = `Recent Temprature is: ${allData[0].temp}`;
-    document.getElementById('content').innerHTML = `And you Feel ${allData[0].content}`;
-    allData =[];
-    document.getElementById('zip').value = " ";
-    document.getElementById('feelings').value = " ";
+    document.getElementById('date').innerHTML = `Date: ${allData.date}`;
+    document.getElementById('temp').innerHTML = `Recent Temprature is: ${Math.round(allData.temp)} F`;
+    document.getElementById('content').innerHTML = `And you Feel ${allData.content}`;
   }
   catch (error) {
     console.log("error", error);
